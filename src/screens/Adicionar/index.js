8b12246fa1d5fc25286d27554df7f8b262b9d708
Davi473 <68740118+Davi473@users.noreply.github.com>
2025-03-06 20:03:37 +0000

    init adicionar

--- src/screens/Adicionar/index.js
+++ src/screens/Adicionar/index.js
@@ -1,42 +1,61 @@
-import { StyleSheet, ScrollView, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, TextInput } from 'react-native';
 import { useState, useEffect } from 'react';
 
 export default function Adicionar(props) {
-    const [retorno, setRetorno] = useState(null);
-    useEffect(() =>
-    {
-        const dados = async () =>
-        {
-            try {
-                const response = await fetch('http://192.168.1.13:3000/months', {
-                    method: 'GET', 
-                    headers: {
-                        'Content-Type': 'application/json',  
-                    },
-                });
-                const data = await response.json(); 
-                setRetorno(data);
-            } catch (e) {
-                console.error('Erro ao fazer GET:', e);
-            }
-        }
-        dados(); 
-    }, []);
+    const [data, setData] = useState();
+
+    // const [retorno, setRetorno] = useState(null);
+    // useEffect(() =>
+    // {
+    //     const dados = async () =>
+    //     {
+    //         try {
+    //             const response = await fetch('http://192.168.1.13:3000/months', {
+    //                 method: 'GET', 
+    //                 headers: {
+    //                     'Content-Type': 'application/json',  
+    //                 },
+    //             });
+    //             const data = await response.json(); 
+    //             setRetorno(data);
+    //         } catch (e) {
+    //             console.error('Erro ao fazer GET:', e);
+    //         }
+    //     }
+    //     dados(); 
+    // }, []);
   
     return (
         <ScrollView style={styles.container}
           showsVerticalScrollIndicator={false}
         >
-        {
-            retorno ?
-                <View> 
-                    <Text>Tela Adicionar</Text>
+            <View> 
+                <View>
+                    <View style={styles.card}>
+                        <View style={styles.cardTitle}>
+                            <Text style={styles.textTitle}>Adicinar</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.formLabel}>Descrição</Text>
+                            <TextInput
+                                style={styles.input}
+                                // onChangeText={setWeight}
+                                // value={weight}
+                                placeholder="Descrição"
+                                keyboardType="ascii-capable"
+                            />
+                            <Text style={styles.formLabel}>Data</Text>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={(text) => setData(text)}
+                                value={data}
+                                placeholder="DD/MM/AAAA"
+                                keyboardType="numeric"
+                            />
+                        </View>
+                    </View>
                 </View>
-            : 
-                <View style={styles.carregando}>
-                    <Text>Carregando...</Text>
-                </View>
-        }
+            </View>
         </ScrollView>
     );
 }
@@ -45,6 +64,42 @@ const styles = StyleSheet.create({
     container: {
         padding: 20,
         marginTop: 0,
+    },
+    card: {
+        width: '90%',
+        borderRadius: 12,
+        backgroundColor: '#ffffff',
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 6,
+        elevation: 5,
+        alignItems: 'center',
+        flexGrow: 1,
+        width: "auto",
+    },
+    cardTitle: {
+        marginTop: 10,
+        marginBottom: 10,
+        justifyContent: 'center', 
+    },
+    textTitle: {
+        color: "#000000",
+        fontSize: 24,
+        paddingLeft: 20,
+    },
+    formLabel: {
+        color: "#000000",
+        fontSize: 18,
+        paddingLeft: 20,
+    },
+    input: {
+        width: "90%",
+        borderRadius: 12,
+        backgroundColor: "#f6f6f6",
+        height: 40,
+        margin: 12,
+        paddingLeft: 10,
     },
     carregando: {
         justifyContent: 'center', 
